0d9a7d39cd2e2bca6ae963d9df4383d40c514cb3
Chawasit Tengtrairatana <chawasit555@gmail.com>
2024-06-16 05:38:02 +0700

    Initial commit

---
+++ app/src/main/java/coffee/hh/hbpeak/MainActivity.kt
@@ -0,0 +1,21 @@
+package coffee.hh.hbpeak
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import coffee.hh.hbpeak.HBPeakNavHost
+import coffee.hh.hbpeak.theme.HBPeakTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            HBPeakTheme {
+                HBPeakNavHost()
+            }
+        }
+    }
+}
